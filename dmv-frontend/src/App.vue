2130df4be96298c17763f095a957d5970db14e64
Amber Phair <amphair@outlook.com>
2019-06-12 20:04:43 -0400

    Initial project setup

---
+++ dmv-frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>DMV Tech Companies</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      stuff
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
